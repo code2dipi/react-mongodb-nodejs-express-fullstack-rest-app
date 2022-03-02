293e6e04927b624e01bf4d7c0085794c4cdb7403
code2dipi <dipineupane@gmail.com>
2022-03-02 21:11:36 +0100

    index file on clinet side has been changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as BrowserRouter, Routes,Route } from 'react-router-dom';
+
+//import {BrowserRouter as Router,Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import NoPage from "./components/NoPage";
+import CreatePost from "./components/CreatePost";
+import CreateUser from "./components/CreateUser";
+import EditPost from "./components/EditPost";
+import PostList from "./components/PostList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <div className="container">
+      <Navbar/>
+      <br/>
+      <Routes>
+      <Route path="/" element={<PostList/>} />
+      <Route path="/edit/:id" element={<EditPost/>} />
+      <Route path="/create" element={<CreatePost/>} />
+      <Route path="/user" element={<CreateUser/>} />
+      <Route path="*" element={<NoPage />}/> 
+      </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
